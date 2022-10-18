593117844ef4f065eedb2242bdcf7a71143a859e
Zaheer Khan Niazi <zaheerniazipak@gmail.com>
2022-10-18 09:17:52 +0500

    Create a Post class and instantiate objects

---
+++ app.py
@@ -0,0 +1,7 @@
+from models.post import Post
+
+post1 = Post("Pakistan", "Pakistan is our dear homeland", "Zaheer")
+
+print("Title: ", post1.title)
+print("Content: ", post1.content)
+print("Author: ", post1.author)
---
+++ models/post.py
@@ -0,0 +1,5 @@
+class Post(object):
+    def __init__(self, title, content, author):
+        self.title = title
+        self.content = content
+        self.author = author
