eed1cbca976052f139ecb2971a51830069e0331f
Zaheer Khan Niazi <zaheerniazipak@gmail.com>
2022-10-18 13:04:30 +0500

    blog class and verifing methods

--- app.py
+++ app.py
@@ -1,18 +1,17 @@
 import pprint
 from database import Database
-from models.post import Post
+from models.blog import Blog
 
 # Database Interaction
 Database.initialize()
 
-# Verify all post methods
+blog = Blog(author="Zaheer",
+            title="Sample Title",
+            description="Sample Description")
 
-# post = Post(blog_id='123', title="Another great post", content="This is some sample content", author="Zaheer")
-# post.save_to_mongo()
+# Verify all Blog methods
+blog.new_post()
+blog.save_to_mongo()
 
-# post = Post.from_mongo('b0cb7d3411e44a0d927ea9e04fdfe92c')
-# pprint.pprint(post)
-
-posts = Post.from_blog('123')
-for post in posts:
-    pprint.pprint(post)
+from_database = Blog.from_mongo(blog.id)
+pprint.pprint(blog.get_posts())
