b746edf4c55f795dda18a071b528905098027cc7
Zaheer Khan Niazi <zaheerniazipak@gmail.com>
2022-10-18 10:31:23 +0500

    Generating random id using uuid python library

--- models/post.py
+++ models/post.py
@@ -1,23 +1,19 @@
-# Blueprint
+import uuid
 from database import Database
 
 
 class Post(object):
-    # Constructor
-    def __init__(self, blog_id, title, content, author, date, id):
-        # Dynamic class Attributes | Properties
+    def __init__(self, blog_id, title, content, author, date, id=None):
         self.blog_id = blog_id
         self.title = title
         self.content = content
         self.author = author
         self.created_date = date
-        self.id = id
+        self.id = uuid.uuid4().hex if id is None else id
 
-    # Method
     def save_to_mongo(self):
         Database.insert(collection='posts', data=self.json())
 
-    # Method
     def json(self):
         return {
             'id': self.id,
