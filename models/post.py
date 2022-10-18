7f7c46da91c9b40380408396ea75e227c39623e8
Zaheer Khan Niazi <zaheerniazipak@gmail.com>
2022-10-18 10:36:53 +0500

    Get current Date Time using datetime module

--- models/post.py
+++ models/post.py
@@ -1,9 +1,10 @@
+import datetime
 import uuid
 from database import Database
 
 
 class Post(object):
-    def __init__(self, blog_id, title, content, author, date, id=None):
+    def __init__(self, blog_id, title, content, author, date=datetime.datetime.utcnow(), id=None):
         self.blog_id = blog_id
         self.title = title
         self.content = content
